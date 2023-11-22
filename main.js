84f48f687f42ac3b6ed8b54bc1d9aa15f06d0fe4
bugsbummer <151781412+bugsbummer@users.noreply.github.com>
2023-11-22 16:11:06 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-var authorization = "Bearer ***"; // replace by authorization value
+var authorization = "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA"; // replace by authorization value
 var ua = navigator.userAgentData.brands.map(brand => `"${brand.brand}";v="${brand.version}"`).join(', ');
-var client_tid = "***"; // replace by X-Client-Transaction-Id value
-var client_uuid = "***"; // replace by X-Client-Uuid value
+var client_tid = "XBLJKHliBnURkEtqvrsZ56on6mvBiuFFnhdR9WnXij/fCgaquQrLqqc5+/jtGM2znK+xUl2dpgz2de+QeZi6+QUiBALcXQ"; // replace by X-Client-Transaction-Id value
+var client_uuid = "3bbcbdff-eacd-488d-9b74-70085e68b0f6"; // replace by X-Client-Uuid value
 var csrf_token = getCookie("ct0");
 var random_resource = "uYU5M2i12UhDvDTzN6hZPg";
 var random_resource_old_tweets = "H8OOoI-5ZE4NxgRr8lfyWg"
 var language_code = navigator.language.split("-")[0]
 var tweets_to_delete = []
 var user_id = getCookie("twid").substring(4);
-var username = "YourUsernameHere" // replace with your username
+var username = "bugsbummer" // replace with your username
 var stop_signal = undefined
 
 var delete_options = {
